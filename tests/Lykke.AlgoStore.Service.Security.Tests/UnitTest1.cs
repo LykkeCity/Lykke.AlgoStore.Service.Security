77cf26ece5dc2336c1c8014934f1a8f8d047df0a
Miladin Joksic <miladin.joksic@primeholding.com>
2018-04-19 15:16:13 +0200

    Removed xUnit and introduced nUnit to tests

--- tests/Lykke.AlgoStore.Service.Security.Tests/UnitTest1.cs
+++ tests/Lykke.AlgoStore.Service.Security.Tests/UnitTest1.cs
@@ -1,12 +1,13 @@
-﻿using Xunit;
+﻿using NUnit.Framework;
 
 namespace Lykke.AlgoStore.Service.Security.Tests
 {
     public class UnitTest1
     {
-        [Fact]
+        [Test]
         public void Test1()
         {
+            Assert.AreEqual(true, true);
         }
     }
 
