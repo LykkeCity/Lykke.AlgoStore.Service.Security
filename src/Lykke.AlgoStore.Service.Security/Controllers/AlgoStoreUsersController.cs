7a59024f00468d785d425355ebda2c43623101a1
Miladin Joksic <miladin.joksic@primeholding.com>
2018-05-31 12:01:21 +0200

    Introduced resource file to store messages

--- src/Lykke.AlgoStore.Service.Security/Controllers/AlgoStoreUsersController.cs
+++ src/Lykke.AlgoStore.Service.Security/Controllers/AlgoStoreUsersController.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Threading.Tasks;
 using Lykke.AlgoStore.Service.Security.Core.Domain;
 using Lykke.AlgoStore.Service.Security.Core.Services;
+using Lykke.AlgoStore.Service.Security.Services.Strings;
 using Lykke.Common.Api.Contract.Responses;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -36,7 +37,7 @@ namespace Lykke.AlgoStore.Service.Security.Controllers
         public async Task<IActionResult> GetUserByIdWithRoles(string clientId)
         {
             if (string.IsNullOrEmpty(clientId))
-                return BadRequest(ErrorResponse.Create("Client Id cannot be empty"));
+                return BadRequest(ErrorResponse.Create(Phrases.ClientIdEmpty));
 
             var result = await _userRolesService.GeyUserByIdWithRoles(clientId);
 
---
+++ tests/Lykke.AlgoStore.Service.Security.Tests/Unit/ResourcesTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Resources;
+using NUnit.Framework;
+
+namespace Lykke.AlgoStore.Service.Security.Tests.Unit
+{
+    [TestFixture]
+    public class ResourcesTests
+    {
+        private ResourceManager _resourceManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var localizationAssembly = Assembly.Load("Lykke.AlgoStore.Service.Security.Services");
+            _resourceManager = new ResourceManager("Lykke.AlgoStore.Service.Security.Services.Strings.Phrases", localizationAssembly);
+        }
+
+        [Test]
+        public void GetStringFromExternalResourceWillReturnValidData()
+        {
+            var clientIdEmptyResourceMessage = _resourceManager.GetString("ClientIdEmpty");
+
+            Assert.AreEqual(clientIdEmptyResourceMessage, "ClientId cannot be empty");
+        }
+    }
+}
