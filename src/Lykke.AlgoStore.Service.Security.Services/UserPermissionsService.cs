e6a62ce0b8633ad10da2014078f072ceba7f73c8
Miladin Joksic <miladin.joksic@primeholding.com>
2018-06-11 13:34:55 +0200

    Removed unused directives

--- src/Lykke.AlgoStore.Service.Security.Services/UserPermissionsService.cs
+++ src/Lykke.AlgoStore.Service.Security.Services/UserPermissionsService.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Lykke.AlgoStore.Service.Security.Core;
 using Lykke.AlgoStore.Service.Security.Core.Domain;
 using Lykke.AlgoStore.Service.Security.Core.Repositories;
 using Lykke.AlgoStore.Service.Security.Core.Services;
